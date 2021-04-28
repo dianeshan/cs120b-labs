dae899c115799c362c7e2e000db6745e1398ee85
diane <dianeshan40785@gmail.com>
2021-04-27 20:41:22 -0700

    Complete part 1

--- test/tests.py
+++ test/tests.py
@@ -15,7 +15,7 @@
 # that they are not ran in isolation but in the order shown and the state of the device is not reset or 
 # altered in between executions (unless preconditions are used).
 tests = [ {'description': 'This test will run first.',
-    'steps': [ {'inputs': [('PINA',<val>)], 'iterations': 1 } ],
+    'steps': [ {'inputs': [('PINA', )], 'iterations': 1 } ],
     'expected': [('PORT',<val>)],
     },
     {'description': 'This test will run second.',
