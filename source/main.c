fddb3d3ef4338d75106fa76a72cb73efa8118f34
diane <dianeshan40785@gmail.com>
2021-05-07 16:15:35 -0700

    Temp

--- source/main.c
+++ source/main.c
@@ -45,44 +45,44 @@ void PWM_off() {
 	TCCR3B = 0x00;
 }
 
-volatile unsigned char TimerFlag = 0;
+//volatile unsigned char TimerFlag = 0;
 
-unsigned long _avr_timer_M = 1;
-unsigned long _avr_timer_cntcurr = 0;
+//unsigned long _avr_timer_M = 1;
+//unsigned long _avr_timer_cntcurr = 0;
 
-void TimerOn(){
-	TCCR1B = 0x0B;
-	OCR1A = 125;
-	TIMSK1 = 0x02;
-	TCNT1 = 0;
-	_avr_timer_cntcurr = _avr_timer_M;
-	SREG |= 0x80;
-}
+//void TimerOn(){
+//	TCCR1B = 0x0B;
+//	OCR1A = 125;
+//	TIMSK1 = 0x02;
+//	TCNT1 = 0;
+//	_avr_timer_cntcurr = _avr_timer_M;
+//	SREG |= 0x80;
+//}
 
-void TimerOff(){
-	TCCR1B = 0x00;
-}
+//void TimerOff(){
+//	TCCR1B = 0x00;
+//}
 
-void TimerISR() {
-	TimerFlag = 1;
-}
+//void TimerISR() {
+//	TimerFlag = 1;
+//}
 
-ISR(TIMER1_COMPA_vect) {
-	_avr_timer_cntcurr--;
-	if(_avr_timer_cntcurr == 0){
-		TimerISR();
-		_avr_timer_cntcurr = _avr_timer_M;
-	}
-}
+//ISR (TIMER1_COMPA_vect) {
+//	_avr_timer_cntcurr--;
+//	if(_avr_timer_cntcurr == 0){
+//		TimerISR();
+//		_avr_timer_cntcurr = _avr_timer_M;
+//	}
+//}
 
-void TimerSet(unsigned long M){
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
-} 
+//void TimerSet(unsigned long M){
+//	_avr_timer_M = M;
+//	_avr_timer_cntcurr = _avr_timer_M;
+//} 
 
 double seqOfNotes [] = { 293.66, 493.88, 440, 392, 293.66, 293.66, 293.66, 493.88, 440, 392, 329.63, 329.63 };
-unsigned char noteTime [] = { 300, 300, 200, 300, 400, 200, 200, 300, 200, 200, 400, 200 };
-unsigned char downTime [] = { 100, 100, 100, 200, 300, 100, 100, 200, 100, 200, 300, 0 };
+double noteTime [] = { 300, 300, 200, 300, 400, 200, 200, 300, 200, 200, 400, 200 };
+double downTime [] = { 100, 100, 100, 200, 300, 100, 100, 200, 100, 200, 300, 0 };
 
 unsigned char i = 0;
 
@@ -90,7 +90,7 @@ enum BT_States { BT_Start, BT_Off, BT_Note, BT_Down, BT_Buttonr } BT_State;
 
 void bt_tick() {
     unsigned char tmpA = ~PINA & 0x01; //button input
-    unsgined char tick = 100; //100 ms
+    unsigned char tick = 100; //100 ms
     unsigned char playTime = 0; //note time array
     unsigned char pauseTime = 0; //down time array
 
@@ -110,7 +110,7 @@ void bt_tick() {
 
         case BT_Note:
             playTime = playTime + tick;
-            if (downTime == 0 || i = 11) {
+            if (downTime[i] == 0 || i == 11) {
                 if (tmpA == 0x01) {
                     BT_State = BT_Buttonr;
                 }
@@ -201,16 +201,16 @@ int main(void) {
 
     /* Insert your solution below */
 
-    TimerSet(100);
-    TimerOn();
+  //  TimerSet(100);
+  //  TimerOn();
     PWM_on();
  
     BT_State = BT_Start;
 
     while(1) {
     	bt_tick();
-    	while (!TimerFlag);
-    	TimerFlag = 0;
+//    	while (!TimerFlag);
+//    	TimerFlag = 0;
     }	
     PWM_off();
     return 1;
