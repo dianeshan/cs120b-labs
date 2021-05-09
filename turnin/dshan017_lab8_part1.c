279a9b00a8eedccc6ad74e246fe58a356c34ddc1
dianethepenguin <59817704+dianethepenguin@users.noreply.github.com>
2021-05-08 21:25:11 -0700

    Add demo link

--- turnin/dshan017_lab8_part1.c
+++ turnin/dshan017_lab8_part1.c
@@ -7,7 +7,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *	
- *	Demo Link:
+ *	Demo Link: https://drive.google.com/file/d/1CNmPU7tmtEUMfA52bZ7YIr-N2MV-urnu/view?usp=drivesdk
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
